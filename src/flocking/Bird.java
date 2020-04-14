cf8882a436d846cfabd00efbe22c1f83f258bced
Chee Jun Wong <cheejunwong@hotmail.com>
2020-04-15 05:31:59 +0800

    created bird.java

---
+++ src/flocking/Bird.java
@@ -0,0 +1,23 @@
+/*
+ * Edit history:
+ *   Greyson, 4/1: created
+ *   
+ *
+*/
+package flocking;
+
+import simstation.Agent;
+
+public class Bird extends Agent {
+
+	public Bird() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
