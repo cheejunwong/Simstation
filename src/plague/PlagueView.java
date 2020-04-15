d847c68230a716a28a149273d10a1ffcb2540899
Chee Jun Wong <cheejunwong@hotmail.com>
2020-04-15 18:33:57 +0800

    completed plague customization

---
+++ src/plague/PlagueView.java
@@ -0,0 +1,35 @@
+package plague;
+
+import simstation.Agent;
+import simstation.SimulationView;
+
+import java.awt.*;
+
+public class PlagueView extends SimulationView {
+
+    public PlagueView(PlagueSimulation model) {
+        super(model);
+    }
+
+    @Override
+    protected void paintComponent(Graphics gc) {
+        PlagueSimulation plagueSimulation = (PlagueSimulation) model;
+        Color oldColor = gc.getColor();
+
+        for (Agent agent : getAgents()) {
+
+            Host plagueAgent = (Host) agent;
+
+            //paint the healthy patients
+            if (plagueAgent.isInfected())
+                gc.setColor(Color.RED);
+            else
+                gc.setColor(Color.GREEN);
+
+            int r = 10;
+            gc.fillOval(agent.getXc(), agent.getYc(), r, r);
+        }
+
+        gc.setColor(oldColor);
+    }
+}
